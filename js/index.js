89178ac909398b6d4e6eb8b4b1c9f4ea934d1b2a
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2024-09-18 17:26:51 +0545

    feat: redesign

--- js/index.js
+++ js/index.js
@@ -605,24 +605,24 @@ $('.services-row').slick({
   slidesToScroll: 1,
   prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
   nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>',
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  // responsive: [
+  //   {
+  //     breakpoint: 1200,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 1,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  // ]
 });
 
 
@@ -636,24 +636,24 @@ $('.why-us-slider').slick({
   slidesToScroll: 1,
   prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
   nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>',
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  // responsive: [
+  //   {
+  //     breakpoint: 1200,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 1,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 992,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  // ]
 });
 
 
